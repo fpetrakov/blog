77776a69eff8b62681094a552f4b8529449cefdc
fpetrakov <jakephin2015@gmail.com>
2022-10-27 21:29:31 +0300

    add description, links and other stuff

--- main.tsx
+++ main.tsx
@@ -3,7 +3,29 @@
 import blog from "blog";
 
 blog({
-  avatar: "https://avatars.githubusercontent.com/u/34133492?s=400&u=d436989e9a8bec2dc2e049c6f2dcfc38af7606dc&v=4",
-  avatarClass: "",
-  author: "Fedya Petrakov",
+	avatar: "https://avatars.githubusercontent.com/u/34133492?s=400&u=d436989e9a8bec2dc2e049c6f2dcfc38af7606dc&v=4",
+	title: "Fedya Petrakov",
+	description: "Hi, I'm here to rant",
+	avatarClass: "border-none rounded-full",
+	author: "Fedya Petrakov",
+	lang: "en",
+  dateStyle: "long",
+	links: [
+		{
+			title: "Twitter",
+			url: "https://twitter.com/fedya_petrakov",
+		},
+		{
+			title: "GitHub",
+			url: "https://github.com/fpetrakov",
+		},
+		{
+			title: "Telegram",
+			url: "https://t.me/fpetrakov",
+		},
+		{
+			title: "Email",
+			url: "mailto:fpetrakov.dev@gmail.com",
+		},
+	],
 });
